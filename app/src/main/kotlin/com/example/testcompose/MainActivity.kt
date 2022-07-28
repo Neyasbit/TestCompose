e634468f65b83809066eaf0fed7ebbd461a75551
neyasbit <kobzevjob@gmail.com>
2022-07-28 11:58:39 +0700

    lesson one

--- app/src/main/kotlin/com/example/testcompose/MainActivity.kt
+++ app/src/main/kotlin/com/example/testcompose/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android Test")
+                    Greeting("Oleg")
                 }
             }
         }
@@ -31,13 +34,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Green) {
+        Text(text = "Hi, my name is $name!", modifier = Modifier.padding(all = 24.dp))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TestComposeTheme {
-        Greeting("Android")
+        Greeting("Oleg")
     }
 }
